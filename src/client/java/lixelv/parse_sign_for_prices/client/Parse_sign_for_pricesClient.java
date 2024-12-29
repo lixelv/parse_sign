e209451abc39b4362dae15702aaf6b039f618245
lixel_ <133605174+lixelv@users.noreply.github.com>
2024-12-29 10:13:53 +0300

    1.0 version

---
+++ src/client/java/lixelv/parse_sign_for_prices/client/Parse_sign_for_pricesClient.java
@@ -0,0 +1,10 @@
+package lixelv.parse_sign_for_prices.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Parse_sign_for_pricesClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/lixelv/parse_sign_for_prices/Parse_sign_for_prices.java
@@ -0,0 +1,10 @@
+package lixelv.parse_sign_for_prices;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Parse_sign_for_prices implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
